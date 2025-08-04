7efd715e9c8f62f8b7d3e29bb750edf8b7200650
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-04 19:59:51 +0530

    Initialize AndroidPhotoPickerService and integrate AndroidApp composable in MainActivity

--- android-app/src/main/kotlin/imager/android/app/MainActivity.kt
+++ android-app/src/main/kotlin/imager/android/app/MainActivity.kt
@@ -3,12 +3,19 @@ package imager.android.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import imager.frontend.shared.AndroidApp
+import imager.frontend.shared.service.AndroidPhotoPickerService
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initializeServices()
         setContent {
-
+            AndroidApp()
         }
+    }
+
+    private fun initializeServices() {
+        AndroidPhotoPickerService.initialize(activity = this)
     }
 }
