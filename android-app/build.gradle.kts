a7ff728627fe5d478bd55dc97e8cdff8ab1be988
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-04 19:25:25 +0530

    Configure android block in android-app module build.gradle.kts

--- android-app/build.gradle.kts
+++ android-app/build.gradle.kts
@@ -7,5 +7,25 @@ plugins {
 group = libs.versions.androidApp.group.get()
 version = libs.versions.androidApp.version.get()
 
+android {
+    namespace = libs.versions.androidApp.namespace.get()
+    compileSdk = libs.versions.androidApp.compileSdk.get().toInt()
+
+    defaultConfig {
+        applicationId = libs.versions.androidApp.applicationId.get()
+        minSdk = libs.versions.androidApp.minSdk.get().toInt()
+        targetSdk = libs.versions.androidApp.targetSdk.get().toInt()
+        versionCode = libs.versions.androidApp.versionCode.get().toInt()
+        versionName = libs.versions.androidApp.versionName.get()
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
 // Custom build directory
 layout.buildDirectory.set(file(path = "$rootDir/.build/AndroidApp"))
