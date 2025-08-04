4f153015f3ffefb38a2238206bc0fdc501e819b0
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-04 19:59:42 +0530

    Add AndroidApp composable to compose-app module

---
+++ compose-app/src/androidMain/kotlin/imager/frontend/shared/AndroidApp.kt
@@ -0,0 +1,9 @@
+package imager.frontend.shared
+
+import androidx.compose.runtime.Composable
+import imager.frontend.shared.presentation.ImageScreen
+
+@Composable
+fun AndroidApp() {
+    ImageScreen()
+}
