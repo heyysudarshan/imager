53380c78382c44187a814f8a3948d939adf12e10
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-04 19:49:07 +0530

    Add MainActivity to android-app module

---
+++ android-app/src/main/kotlin/imager/android/app/MainActivity.kt
@@ -0,0 +1,14 @@
+package imager.android.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
