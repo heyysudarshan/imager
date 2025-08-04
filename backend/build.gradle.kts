35b0bfbed525628e3180a1a943583047b6e4fb0b
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-04 17:09:47 +0530

    Set Java 21 compatibility and update Kotlin JVM target in backend build configuration

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,9 +1,23 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.kotlin.compiler)
 }
 
 group = libs.versions.backend.group.get()
 version = libs.versions.backend.version.get()
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of("21"))
+    }
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+kotlin.compilerOptions {
+    jvmTarget = JvmTarget.JVM_21
+}
 
 // Custom build directory
 layout.buildDirectory.set(file(path = "$rootDir/.build/Backend"))
