67be3985ee07f3de07396b8f93048c163e22cf30
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-04 18:14:40 +0530

    Enable Kotlin Multiplatform support in the compose-app module

--- compose-app/build.gradle.kts
+++ compose-app/build.gradle.kts
@@ -3,5 +3,20 @@ plugins {
     alias(libs.plugins.android.library)
 }
 
+kotlin {
+    androidTarget()
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "ComposeApp"
+            isStatic = true
+        }
+    }
+}
+
 // Custom build directory
 layout.buildDirectory.set(file(path = "$rootDir/.build/ComposeApp"))
