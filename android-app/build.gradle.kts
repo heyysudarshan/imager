4e2dede4369c74dea4569f70e9d87fe17c0f3647
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-04 19:24:23 +0530

    Set group and version for android-app module in build.gradle.kts

--- android-app/build.gradle.kts
+++ android-app/build.gradle.kts
@@ -4,5 +4,8 @@ plugins {
     alias(libs.plugins.compose.compiler)
 }
 
+group = libs.versions.androidApp.group.get()
+version = libs.versions.androidApp.version.get()
+
 // Custom build directory
 layout.buildDirectory.set(file(path = "$rootDir/.build/AndroidApp"))
