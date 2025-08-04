9d6bb28b887036e7323550b8b7aa7af121d306ab
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-04 19:12:35 +0530

    Add "androidx.lifecycle:lifecycle-viewmodel-compose" dependency to the compose-app module

--- compose-app/build.gradle.kts
+++ compose-app/build.gradle.kts
@@ -24,6 +24,7 @@ kotlin {
 
     sourceSets {
         commonMain.dependencies {
+            implementation(libs.lifecycle.viewmodel.compose)
             implementation(libs.compose.multiplatform.material3)
             implementation(libs.compose.multiplatform.ui)
             implementation(libs.compose.multiplatform.foundation)
