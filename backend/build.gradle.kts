828d6e9c3a2f7214f23c1ce748ecdfa52c50a32d
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-04 17:08:00 +0530

    Configure group and version for the backend module in build.gradle.kts

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -2,5 +2,8 @@ plugins {
     alias(libs.plugins.kotlin.compiler)
 }
 
+group = libs.versions.backend.group.get()
+version = libs.versions.backend.version.get()
+
 // Custom build directory
 layout.buildDirectory.set(file(path = "$rootDir/.build/Backend"))
