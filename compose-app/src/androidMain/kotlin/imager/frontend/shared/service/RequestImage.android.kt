914281ce7abd3c9ee6f3890499c576dfb2337269
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-04 20:21:56 +0530

    Simplify `requestImage` by removing redundant code and adjusting callback bindings

--- compose-app/src/androidMain/kotlin/imager/frontend/shared/service/RequestImage.android.kt
+++ compose-app/src/androidMain/kotlin/imager/frontend/shared/service/RequestImage.android.kt
@@ -3,14 +3,9 @@ package imager.frontend.shared.service
 internal actual fun requestImage(
     onPhotoPicked: (ByteArray) -> Unit,
     onPhotoPickerError: () -> Unit
-): ByteArray {
+) {
     AndroidPhotoPickerService.pickPhoto(
-        onPhotoPicked = {
-            onPhotoPicked(it)
-        },
-        onPhotoPickerError = {
-            onPhotoPickerError()
-        }
+        onPhotoPicked = onPhotoPicked,
+        onPhotoPickerError = onPhotoPickerError
     )
-    return ByteArray(0)
 }
