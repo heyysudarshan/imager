b7de1c8e708bdbf18cc1f6014ffd029ca8af25a3
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-04 18:02:01 +0530

    Handle image upload and save functionality in `BackendApplication` `/upload` endpoint

--- backend/src/main/kotlin/imager/backend/app/BackendApplication.kt
+++ backend/src/main/kotlin/imager/backend/app/BackendApplication.kt
@@ -35,15 +35,37 @@ class BackendApplication {
             )
         }
 
-        return Response(
-            status = "Failed",
-            message = ResponseMessage(
-                message = body.image,
-                statusCode = 400
+        // Try to save image
+        try {
+            // Create a file upload directory
+            Files.createDirectories(Paths.get(uploadDir))
+
+            val imageExtension = when (imageType) {
+                "image/jpeg" -> "jpeg"
+                else -> "png"
+            }
+
+            val fileName = "img_${UUID.randomUUID()}.png"
+            val filePath = Paths.get(uploadDir, fileName)
+
+            Files.write(filePath, body.image)
+
+            return Response(
+                status = "Image uploaded successfully.",
+                message = ResponseMessage(
+                    message = "Success",
+                    statusCode = 200
+                )
             )
-        )
-
-
+        } catch (_: Exception) {
+            return Response(
+                status = "Image upload failed.",
+                message = ResponseMessage(
+                    message = "Failed",
+                    statusCode = 400
+                )
+            )
+        }
     }
 }
 
