dbc74f8cb134ab2fedf2eea8e3c6581ce7103c85
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-05 21:04:15 +0530

    Update IosApp.kt and ContentView.swift

--- compose-app/src/iosMain/kotlin/imager/frontend/shared/IosApp.kt
+++ compose-app/src/iosMain/kotlin/imager/frontend/shared/IosApp.kt
@@ -1,1 +1,6 @@
 package imager.frontend.shared
+
+import androidx.compose.ui.window.ComposeUIViewController
+import imager.frontend.shared.presentation.ImageScreen
+
+fun IosViewController() = ComposeUIViewController { ImageScreen() }
