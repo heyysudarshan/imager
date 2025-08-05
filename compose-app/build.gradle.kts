453bf7b46d0cb86c349fba61195786a7b78075a3
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-05 22:07:32 +0530

    Remove custom build directory configuration from `compose-app`

--- compose-app/build.gradle.kts
+++ compose-app/build.gradle.kts
@@ -43,6 +43,3 @@ android {
         minSdk = libs.versions.composeApp.androidLibrary.minSdk.get().toInt()
     }
 }
-
-// Custom build directory
-layout.buildDirectory.set(file(path = "$rootDir/.build/ComposeApp"))
