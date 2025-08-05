fce33d2fa90c424903460fa24834f6ee232fa6a2
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-05 19:54:58 +0530

    Add `showUploadImageScreen` property to `ImageScreenUiState` with default value

--- compose-app/src/commonMain/kotlin/imager/frontend/shared/presentation/ImageScreenUiState.kt
+++ compose-app/src/commonMain/kotlin/imager/frontend/shared/presentation/ImageScreenUiState.kt
@@ -2,6 +2,7 @@ package imager.frontend.shared.presentation
 
 @Suppress("ArrayInDataClass")
 internal data class ImageScreenUiState(
+    val showUploadImageScreen: Boolean = true,
     val isImageSelected: Boolean = false,
     val selectedImage: ByteArray? = null,
     val isUploading: Boolean = false,
